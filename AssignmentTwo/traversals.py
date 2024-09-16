dc0f9b4ce3b00361d2b625454d7f4b51130c1721
CadenWonzer <caden.wonzer@icloud.com>
2024-09-15 22:43:39 -0500

    Revert "all but spiral"
    This reverts commit 416e262cf47af240326fd222129a6370247ee127.

--- AssignmentTwo/traversals.py
+++ AssignmentTwo/traversals.py
@@ -30,128 +30,37 @@ def row_major_traversal(grid):
 # Iterates over a 2D list from left to right, then top to bottom
 # and returning the coordinates (row, column).
 def column_major_traversal(grid):
-    order = []
-    col_len = len(grid[0])
-    for j in range(col_len):
-        for i in range(len(grid)):
-            pair = (i, j)
-            order.append(pair)
-    return order
+    return []
 
 
 # Iterates over a 2D list from top to bottom then left to right
 # and returning the coordinates (row, column).
 def row_zigzag_traversal(grid):
-    order = []
-    for i in range(len(grid)):
-        if i % 2 == 0:
-            for j in range(len(grid[i])):
-                pair = (i, j)
-                order.append(pair)
-        if i % 2 == 1:        
-            for j in range(len(grid[i])-1, -1, -1):
-                pair = (i, j)
-                order.append(pair)
-    return order
+    return []
 
 
 # Iterates over a 2D list by alternating between iterating
 # left to right and right to left, going from top to bottom
 # and returning the coordinates (row, column).
 def column_zigzag_traversal(grid):
-    order = []
-    col_len = len(grid[0])
-    for j in range(col_len):
-        if j % 2 == 0:
-            for i in range(len(grid)):
-                pair = (i, j)
-                order.append(pair)
-        if j % 2 == 1:
-            for i in range(len(grid)-1, -1, -1):
-                pair = (i, j)
-                order.append(pair)
-    return order
+    return []
 
 
 # Iterates over a 2D list from the top-right to the bottom-left
 # in the direction of the main diagonal and returning the
 # coordinates (row, column).
 def main_diagonal_traversal(grid):
-    order = []
-    for j in range(len(grid[0])-1, -1, -1):
-        pair = (0, j)
-        order.append(pair)
-        num = 1
-        while num < len(grid) and j+num < len(grid[0]):
-            pair = (num, j+num)
-            order.append(pair)
-            num+=1
-    for i in range(1, len(grid), 1):
-        pair = (i, 0)
-        order.append(pair)
-        num = 1
-        while i+num < len(grid) and num < len(grid[0]):
-            pair = (i+num, num)
-            order.append(pair)
-            num+=1
-    return order
+    return []
+
 
 # Iterates over a 2D list from the top-left to the bottom-right
 # in the direction of the secondary diagonal and returning the
 # coordinates (row, column).
 def secondary_diagonal_traversal(grid):
-    order = []
-    for j in range(0, len(grid[0]), 1):
-        pair = (0, j)
-        order.append(pair)
-        num = 1
-        while num < len(grid) and j-num > -1:
-            pair = (num, j-num)
-            order.append(pair)
-            num+=1
-    for i in range(1, len(grid), 1):
-        pair = (i, len(grid[0])-1)
-        order.append(pair)
-        num = 1
-        while i+num < len(grid) and len(grid[0])-1-num > -1:
-            pair = (i+num, len(grid[0])-1-num)
-            order.append(pair)
-            num+=1
-    return order
+    return []
 
 
 # Iterates over a 2D list in spiral order and returning the
 # coordinates (row, column).
 def spiral_traversal(grid):
-    order = []
-    rows = len(grid)
-    cols = len(grid[0])
-    end_size = len(grid) * len(grid[0])
-    turn = 1
-    for j in range(len(grid[0])):
-        pair = (0, j)
-        order.append(pair)
-    while len(order) < end_size:
-        if turn % 2 == 0:
-            if turn % 4 == 2:
-                for j in range(cols-1, len(grid[0])-cols, -1):
-                    pair = (rows-1, j)
-                    order.append(pair)
-            else:
-                for j in range(len(grid)-rows, rows, 1):
-                    pair = (len(grid[0])-rows-1, j)
-                    order.append(pair)
-            turn+=1
-            cols-=1
-        if turn % 2 == 1:
-            if turn % 4 == 3:
-                for i in range(rows-1, len(grid)-rows, -1):
-                    pair = (i, len(grid[0])-1-cols)
-                    order.append(pair)
-            else:
-                for i in range(len(grid[0])-cols, cols, 1):
-                    pair = (i, cols-1)
-                    order.append(pair)
-            turn+=1
-            rows-=1
-    return order
+    return []
